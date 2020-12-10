c0d5433de5a27e5bedb10e025c7d3f03eeb34101
MitarashiDango <mitarashi_dango@mail.matcha-soft.com>
2020-12-11 04:16:24 +0900

    パラメータ追加

--- src/config.ts
+++ src/config.ts
@@ -2,10 +2,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+export const ACCESS_TOKEN = process.env.ACCESS_TOKEN || "";
 export const SLEEP_TIME = parseInt(process.env.SLEEP_TIME || "2000", 10);
-export const DOMAIN = process.env.DOMAIN;
+export const DOMAIN = process.env.DOMAIN || "";
 export const EXCLUDE_REPLIES =
   (process.env.EXCLUDE_REPLIES || "").trim().toLowerCase() === "true";
 export const EXCLUDE_REBLOGS =
   (process.env.EXCLUDE_REBLOGS || "").trim().toLowerCase() === "true";
+export const MAX_ID = process.env.MAX_ID || "";
+export const MIN_ID = process.env.MIN_ID || "";
--- src/requestors/mastodon.ts
+++ src/requestors/mastodon.ts
@@ -1,5 +1,11 @@
 import { api } from "../api";
-import { DOMAIN, EXCLUDE_REBLOGS, EXCLUDE_REPLIES } from "../config";
+import {
+  DOMAIN,
+  EXCLUDE_REBLOGS,
+  EXCLUDE_REPLIES,
+  MAX_ID,
+  MIN_ID,
+} from "../config";
 import { Account, Post, Requestor } from "../interfaces";
 
 export class MastodonRequestor implements Requestor<Account, Post> {
@@ -11,12 +17,20 @@ export class MastodonRequestor implements Requestor<Account, Post> {
 
   async fetchPostsByAccountId(accountId: string) {
     const url = `https://${DOMAIN}/api/v1/accounts/${accountId}/statuses`;
-    const res = await api().get<Post[]>(url, {
-      params: {
-        exclude_reblogs: EXCLUDE_REBLOGS,
-        exclude_replies: EXCLUDE_REPLIES,
-      },
-    });
+    const params: { [key: string]: any } = {
+      exclude_reblogs: EXCLUDE_REBLOGS,
+      exclude_replies: EXCLUDE_REPLIES,
+    };
+
+    if (MIN_ID !== "") {
+      params["min_id"] = MIN_ID;
+    }
+
+    if (MAX_ID !== "") {
+      params["max_id"] = MAX_ID;
+    }
+
+    const res = await api().get<Post[]>(url, { params });
     return res.data;
   }
 
